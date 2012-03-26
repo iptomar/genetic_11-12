2c0bb0cf47bc99287298b1b6f4d1ed7c341dda69
RubenFelix <Ruben.Felix@gmail.com>
2012-03-26 22:16:38 +0100

    Teste

---
+++ GeneticProject/src/GeneticV01/Gene.java
@@ -0,0 +1,33 @@
+package GeneticV01;
+
+/* -------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ *  I n s t i t u t o   P o l i t e c n i c o   d e   T o m a r
+ *   E s c o l a   S u p e r i o r   d e   T e c n o l o g i a
+ *
+ * @author Ruben Felix <Ruben.Felix@gmail.com>
+ * -------------------------------------------------------------------------
+ * Número de Aluno: 13691 
+ * E-mail: Ruben.Felix@gmail.com
+ * -------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ */
+public class Gene <T>{
+    private T value;
+
+    /**
+     * Método que retorna o valor de T
+     * @return the value
+     */
+    public T getValue() {
+        return value;
+    }
+
+    /**
+     * Método que define o valor de T
+     * @param value the value to set
+     */
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
