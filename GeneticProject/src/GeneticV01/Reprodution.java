17a4331239fe66ea571e500c03f752ebb1c6e415
andresimoes <andre.simoes.7@hotmail.com>
2012-03-28 14:26:09 +0100

    Reprodução

--- GeneticProject/src/GeneticV01/Reprodution.java
+++ GeneticProject/src/GeneticV01/Reprodution.java
@@ -42,9 +42,52 @@ public class Reprodution {
     
     //FALTA IMPLEMENTAÇÃO
     public ArrayList<Individual> execute(){
+        //*******************************************************************************
+        //SÓ FUNCIONA PARA O CASO DOS INDIVIDUOS TEREM APENAS UM CROMOSSOMA NO SEU GENOMA
+        //*******************************************************************************
         int numGenes = populationBeforeRepr.get(0).getChromosome(0).getNumberGenes();
         int trim = numGenes/2;
-        
+        //Criação de dois novos individuos queserão os filhos da reprodução de dois pais, com o mesmo número
+        //de cromossomas e de genes por cromossoma
+        Individual filho1 = new Individual(populationBeforeRepr.get(0).getNumberChromosomes(), numGenes);
+        Individual filho2 = new Individual(populationBeforeRepr.get(0).getNumberChromosomes(), numGenes);
+        //Dois cromossomas novos que serão a junção dos cromossomas para os filhos
+        Chromosome cromFilho1 = new Chromosome(numGenes);
+        Chromosome cromFilho2 = new Chromosome(numGenes);
+        //Caso o número de genes for número par
+        if((trim*2)==numGenes){
+            //Ciclo que percorre todo o array da população inicial
+            for (int i = 0; i < populationBeforeRepr.size(); i++) {
+                //Novos objectos do tipo Individual para receber os novos cromossomas
+                filho1 = new Individual(populationBeforeRepr.get(0).getNumberChromosomes(), numGenes);
+                filho2 = new Individual(populationBeforeRepr.get(0).getNumberChromosomes(), numGenes);
+                //Novos objectos do tipo cromossomas para serem definidos e serem inseridos nos filhos
+                //provenientes da reprodução dos pais
+                cromFilho1 = new Chromosome(numGenes);
+                cromFilho2 = new Chromosome(numGenes);
+                //Copia os genes dos pais para os filhos (a primeira metade)
+                for (int j = 0; j < trim; j++) {
+                    cromFilho1.setGene(j, populationBeforeRepr.get(i).getChromosome(0).getGene(j));
+                    cromFilho2.setGene(j, populationBeforeRepr.get(i+1).getChromosome(0).getGene(trim+j));
+                }
+                //Copia os genes dos pais para os filhos (a segunda metade)
+                for (int j = trim; j < numGenes; j++) {
+                    cromFilho1.setGene(j, populationBeforeRepr.get(i+1).getChromosome(0).getGene(j-trim));
+                    cromFilho2.setGene(j, populationBeforeRepr.get(i).getChromosome(0).getGene(j));
+                }
+                //Percorre o array da população de dois em dois
+                i++;
+                //Faz o set dos cromossomas
+                filho1.setChromosome(0, cromFilho1);
+                filho2.setChromosome(0, cromFilho2);
+                populationAfterRepr.add(filho1);
+                populationAfterRepr.add(filho2);
+            }
+        }
+        //Caso seja impar
+        else{
+            
+        }
         return populationAfterRepr;
     }
 }
