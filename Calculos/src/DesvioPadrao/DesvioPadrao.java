604a25daf2f04740470a977e5076858c57aa9023
andresimoes <andre.simoes.7@hotmail.com>
2012-04-17 02:17:08 +0100

    Classe que calcula a média a variância e o desvio padrão. Falta portanto encaixa-la no projecto e receber no array o fitness dos individuos. Trabalho efectuado por André Simões, Hélio Miranda e André  Pereira.

---
+++ Calculos/src/DesvioPadrao/DesvioPadrao.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DesvioPadrao;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DesvioPadrao {
+
+    double soma = 0;
+    double somatorio;
+    double media;
+    double array[] = {1, 4, 6, 9, 2};
+
+  //metodo que devolve a media
+    public double getMedia() {
+        media = 0;
+        soma = 0;
+
+        for (int counter = 0; counter < array.length; counter++) {
+
+            soma += array[counter];
+        }
+        media = soma / array.length;
+        return media;
+    }
+
+  //metodo que devolve a variância
+    public double getVariancia() {
+        soma = 0;
+        somatorio = 0;
+        //Calcula o somatório
+        for (int counter = 0; counter < array.length; counter++) {
+            somatorio += Math.pow(array[counter] - getMedia(), 2);
+        }
+        double variancia = (somatorio / (array.length - 1));
+        return variancia;
+    }
+
+    //metodo que devolve o desvio padrão
+    public double getDesvioPadrao() {
+
+        return Math.sqrt(getVariancia());
+    }
+
+    
+    public double[] getArray() {
+
+        return array;
+    }
+    
+    public void setArray(double[] array) {
+
+        this.array = array;
+    }
+}
