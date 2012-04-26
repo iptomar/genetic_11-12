5246aa065b682beee71bef191e2fbfa63c0e13b1
AndrePereira76 <muralha_12@hotmail.com>
2012-04-26 16:46:41 +0100

    Classe Teste

---
+++ v0.2/GeneticAlgoritms/test/genetics/GeneTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genetics;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Helio
+ */
+public class GeneTest {
+    
+    public GeneTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test para verificar se os genes correspondem aos introduzidos
+     */
+    @Test
+    public void testGetSetAllele() {
+        //define se uma array de boolean
+        boolean[] alleloTeste = {false, true, true, false};
+        //passa se por parametros
+        Gene<boolean[]> gene = new Gene<boolean[]>(alleloTeste);
+        
+        //verifica se os valores são os mesmo, senao o teste não passa
+        for (int i = 0; i < alleloTeste.length; i++) {
+            if(!alleloTeste[i]==gene.getAllele()[i]){
+                fail("O allelo que foi especificado não é o mesmo que está definido de momento.");
+            }
+        }
+    }
+
+}
