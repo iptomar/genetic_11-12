a1b9be8ba9ed0ae48b3af982e68d2e21c6f43e85
AndrePereira76 <muralha_12@hotmail.com>
2012-04-26 16:35:05 +0100

    Classe de Teste do Tournament, já feito commit mas apagado por alguem

--- v0.2/GeneticAlgoritms/test/operators/replacements/TournamentTest.java
+++ v0.2/GeneticAlgoritms/test/operators/replacements/TournamentTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Pedro
+ * @author André
  */
 public class TournamentTest {
     
@@ -32,14 +32,52 @@ public class TournamentTest {
      */
     @Test
     public void testExecute() {
-        System.out.println("execute");
-        Population parents = null;
-        Population sons = null;
-        Tournament instance = new Tournament();
-        Population expResult = null;
+        System.out.println("-- Teste ao operador Tournament --");
+        /**
+         * População de pais, com 100 individuos e com o alelo a 10.
+         */
+        Population parents = new Population(100, 1, 1, 10, new genetics.algorithms.OnesMax(), true);
+        /**
+         * População de filhos, com 100 individuos e com o alelo a 10.
+         */
+        Population sons = new Population(100, 1, 1, 10, new genetics.algorithms.OnesMax(), true);
+        /**
+         * Criação do torneio de 100 elementos 2 a 2
+         */
+        Tournament instance = new Tournament(100,2);
+        /**
+         * Execução do torneio
+         */
         Population result = instance.execute(parents, sons);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        /**
+         * Compara se o o size da population do result é 100, que é o numero de individos que sai no torneio
+         */
+        assertEquals(result.getPopulation().size(), 100);
+        
+        /**
+         * Ordena os resultados obtidos no torneio
+         */
+        utils.PopulationUtils.orderPopulation(result);
+        utils.PopulationUtils.orderPopulation(parents);
+        utils.PopulationUtils.orderPopulation(sons);
+        /**
+         * Condição para saber se um individuo da população resultante do torneio pertence à população
+         */
+        if(result.getPopulation().get(0).fitness() != parents.getPopulation().get(0).fitness() && result.getPopulation().get(0).fitness() != sons.getPopulation().get(0).fitness()){
+            fail("Um individuo da população resultante do torneio não se encontra na população pai nem na população filho.");
+        }
+        /**
+         * Imprime o torneio, ordenado pelo individuo com melhor fitness
+         */
+        System.out.println(" " + result);
+        /**
+         * Mostra qual o valor de fitness do melhor inviduo
+         * Não é possivel saber se o individuo com melhor fitness pertence 
+         * aos pais ou aos filhos devia ao facto da existencia do método clone na classe Tournament
+         */
+        System.out.println("Melhor individuo: " + result.getPopulation().get(0).fitness());
+        
+        
     }
 }
