40e5eb22fcc4b5d33ea9175478e78fd75d08bfd7
pedroalves <pedroalves_6@hotmail.com>
2012-05-16 23:02:39 +0100

    Ajuste/reposição de material esquecido/apagado..

--- v0.3/GeneticAlgoritms/test/operators/selections/RouletteTest.java
+++ v0.3/GeneticAlgoritms/test/operators/selections/RouletteTest.java
@@ -4,6 +4,8 @@
  */
 package operators.selections;
 
+import utils.PopulationUtils;
+import genetics.algorithms.OnesMax;
 import genetics.Population;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -17,6 +19,16 @@ import static org.junit.Assert.*;
  * @author Ruben Felix <Ruben.Felix@gmail.com>
  */
 public class RouletteTest {
+    
+    
+    private Population p;
+    private Roulette r;
+    int sizePopulation = 10;
+    int sizeGenome = 1;
+    int sizeGenotype = 1;
+    int sizeAllelo = 10;
+    int DimSelectedPopulation = 5;
+    int testeAleatorio = 100;
     
     public RouletteTest() {
     }
@@ -42,13 +54,56 @@ public class RouletteTest {
      */
     @Test
     public void testExecute() {
-        System.out.println("execute");
-        Population population = null;
-        Roulette instance = new Roulette();
-        Population expResult = null;
-        Population result = instance.execute(population);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+//******************    Vê se o ponto aleatório está ente 0 e 1 ->  (0-maxFitness)/maxFitness  *********************************
+       
+        //população gerada
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        //SUS com a dimensão a ser seleccionada
+        r = new Roulette(DimSelectedPopulation);
+        
+         Population popSaida;
+        //executa o Roleta com a população que foi gerada
+        popSaida = r.execute(p);
+        //soma total de fitness de todos os individuos da população
+        int maxFitness = (int) PopulationUtils.getFitnessTotal(p);
+        //variavel com o valor que é gerado aleatoriamente no SUS
+        double pontoAleatorio = r.getPonteiro();
+        
+        
+        
+        assertTrue( 0<= pontoAleatorio && pontoAleatorio <= PopulationUtils.getFitnessTotal(r.getPopIn()));
+     
+//*************** Vê se o numero de individuos da população de saida é igual ao numero seleccionado ***************
+        
+        assertTrue( popSaida.getSizePopulation() == DimSelectedPopulation);
+        
+//*********************Vê se individuos da popOUT fazem parte da popIN***********************************************
+        Population pi = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        Population pf = r.execute(pi);
+
+
+//tpf-testepopfinal
+        //percorrer a pop de saida, e ver se cada individuo está contido na pop de entrada
+        int tpf = pf.getSizePopulation();
+        for (int j = 0; j < tpf; j++) {
+            for (int i = 0; i < pi.getSizePopulation(); i++) {
+                if (pf.getIndividual(0).fitness() == pi.getIndividual(i).fitness() ) {
+                    pf.getPopulation().remove(0);
+                    pf.DecSize();
+                    break;
+                }
+            }
+        }
+
+        
+        boolean t = false;
+        if (pf.getSizePopulation() == 0) {
+            t = true;
+        }
+        
+       assertTrue(t);
+        
+        
     }
 }
