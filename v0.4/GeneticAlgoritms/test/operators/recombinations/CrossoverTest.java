398c5a3d26cdab624cd9ec24b175563687abd231
pedroalves <pedroalves_6@hotmail.com>
2012-06-07 21:00:42 +0100

    Testes finais. Fim dos Testes!!☺
    Ass: Pedro Alves, André Simões e Pedro Isidoro.

--- v0.4/GeneticAlgoritms/test/operators/recombinations/CrossoverTest.java
+++ v0.4/GeneticAlgoritms/test/operators/recombinations/CrossoverTest.java
@@ -4,6 +4,7 @@
  */
 package operators.recombinations;
 
+import genetics.algorithms.OnesMax;
 import genetics.Population;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -14,41 +15,176 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Ruben Felix <Ruben.Felix@gmail.com>
+ * @author Pedro Alves
  */
 public class CrossoverTest {
     
     public CrossoverTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
     
-    @Before
-    public void setUp() {
-    }
     
-    @After
-    public void tearDown() {
-    }
+    
+    int sizePopulation = 5;
+    int sizeGenome = 1;
+    int sizeGenotype = 1;
+    int sizeAllelo = 10;
+    int DimSelectedPopulation = 5;
+    int testeAleatorio = 100;
+    double probabilidade = 0.75;
+    Population p, popEntrada, popSaida;
+    Crossover co;
+    
 
-    /**
-     * Test of execute method, of class Crossover.
-     */
+//##########################################################################################################################
+//###################$$$$$ Testar se existe alteração dos bits dos individuos com probabilidade a 100% $$$$$################
+//##########################################################################################################################
+    
     @Test
-    public void testExecute() {
-        System.out.println("execute");
-        Population population = null;
-        Crossover instance = new Crossover();
-        Population expResult = null;
-        Population result = instance.execute(population);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testAlteracao(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 100%
+        probabilidade=1;
+        co = new  Crossover(probabilidade,1);
+        
+        popSaida=co.execute(p);
+        //vai ver se as populações são diferentes, uma vez que foram mutadas com probabilidade a 100%
+        assertNotSame(popEntrada, popSaida);
+     
     }
+    
+//***************************************************************************************************************************
+    
+    
+//##########################################################################################################################
+//###################***** Testar se existe alteração dos bits dos individuos com probabilidade a 0% *****##################
+//##########################################################################################################################
+    
+    @Test
+    public void testIgualdade(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 0%
+        probabilidade=0*0;
+        co = new  Crossover(probabilidade,1);
+        
+        popSaida=co.execute(p);
+        
+        
+        //vai ver se as populações são iguais, uma vez que foram mutadas com probabilidade a 0%
+        assertEquals(popEntrada.toString() ,popSaida.toString());
+        
+        
+        //System.out.println(""+popEntrada.toString());
+        //System.out.println(""+popSaida.toString());
+     
+    }
+    
+//***************************************************************************************************************************
+    
+    
+//##########################################################################################################################
+//###################!!!!!! Testa se a população de entrada tem o mesmo tamanho da população de saída   !!!!!!!#############
+//##########################################################################################################################
+    
+    @Test
+    public void testTamanhoPopulacao(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 0%
+        probabilidade=0.75;
+        co = new  Crossover(probabilidade,1);
+        
+        popSaida=co.execute(p);
+        //compara o tamanho das populações
+        assertTrue(popEntrada.getSizePopulation() ==popSaida.getSizePopulation());
+     
+    }
+    
+//***************************************************************************************************************************
+    // Vê se com a probabilidade a 0% e com 0 cortes a população se mantem igual    
+    @Test
+    public void testCorte1(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 0%
+        probabilidade=0;
+        co = new  Crossover(probabilidade,0);
+        
+        popSaida=co.execute(p);
+        //compara o tamanho das populações
+        assertTrue(popEntrada.toString().equals(popSaida.toString()));
+     
+    }
+    
+//***************************************************************************************************************************
+   // Vê se com a probabilidade a 0% e com 1 corte a população se mantem igual    
+    @Test
+    public void testCorte2(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 0%
+        probabilidade=0;
+        co = new  Crossover(probabilidade,1);
+        
+        popSaida=co.execute(p);
+        //compara o tamanho das populações
+        assertTrue(popEntrada.toString().equals(popSaida.toString()));
+     
+    }
+    
+//***************************************************************************************************************************
+    // Vê se com a probabilidade a 100% e com 0 cortes a população se mantem igual    
+    @Test
+    public void testCorte3(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 100%
+        probabilidade=1;
+        co = new  Crossover(probabilidade,0);
+        
+        popSaida=co.execute(p);
+        //compara o tamanho das populações
+        assertTrue(popEntrada.toString().equals(popSaida.toString()));
+     
+    }
+    
+//***************************************************************************************************************************
+    // Vê se com a probabilidade a 100% e com 1 corte a população fica diferente    
+    @Test
+    public void testCorte4(){
+       
+        p = new Population(sizePopulation, sizeGenome, sizeGenotype, sizeAllelo, new OnesMax());
+        
+        popEntrada=p;
+        //probabilidade de 100%
+        probabilidade=1;
+        co = new  Crossover(probabilidade,1);
+        
+        popSaida=co.execute(p);
+        //compara o tamanho das populações
+        assertFalse(popEntrada.toString().equals(popSaida.toString()));
+     
+    }
+    
+//***************************************************************************************************************************
+    
+    
+    
+    
+    
+   
 }
